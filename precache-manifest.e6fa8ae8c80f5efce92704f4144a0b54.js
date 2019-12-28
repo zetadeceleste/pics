8116483368840103380eba633b6cd203db865809
Celeste <czapata@vaypol.com.ar>
2019-12-28 11:22:19 -0300

    Updates

---
+++ precache-manifest.e6fa8ae8c80f5efce92704f4144a0b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afc7a86d9546b2fa73243d11f8477fe",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "add21a6e1adadc7703ba",
+    "url": "/pics/static/css/main.744c5823.chunk.css"
+  },
+  {
+    "revision": "4f0eff5d11556fe6478f",
+    "url": "/pics/static/js/2.28613c32.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/pics/static/js/2.28613c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "add21a6e1adadc7703ba",
+    "url": "/pics/static/js/main.ef0905eb.chunk.js"
+  },
+  {
+    "revision": "a486d975594a83fabef1",
+    "url": "/pics/static/js/runtime-main.5707d73b.js"
+  }
+]);
